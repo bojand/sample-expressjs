7f2ea3848bc9e1dd199575f64ce5ddc7c2aa451f
Bojan <dbojan@gmail.com>
2020-11-12 21:48:10 -0400

    add cpu and mem routes

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express')
+const inspector = require('inspector');
+const fs = require('fs');
 const app = express()
 const port = process.env.PORT || 3000
+const timeout = 10000
 
 app.get('/', (req, res) => {
 	res.send('Hello World!')
 })
 
 app.listen(port, () => console.log(`sample-expressjs app listening on port ${port}!`))
+
+app.get('/cpu', (req, res) => {
+	const startUsage = process.cpuUsage();
+
+	setTimeout(() => {
+		res.json(process.cpuUsage(startUsage));
+	}, timeout);
+})
+
+app.get('/mem', (req, res) => {
+	res.json(process.memoryUsage());
+})
